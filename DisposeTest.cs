8f7446d9d2d9af9b872687d2ccf27521aa95af74
Егор Богданов <egor5803@bk.ru>
2022-03-03 14:38:33 +0300

    new

---
+++ DisposeTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Qqq
+{
+    internal class DisposeTest : IDisposable
+    {
+
+        public void SomeMethod()
+        {
+            Action act = () => Console.WriteLine("QQQQQQQQ");
+
+            act.Invoke();
+        }
+
+        public void Dispose() 
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                Console.WriteLine("Освободил управляемые ресурсы!");
+                return;
+            }
+            Console.WriteLine("Ууу... Освобождаю неуправляемые ресурсы!");
+            disposing = true;
+        }
+        ~DisposeTest()
+        {
+            Dispose(false);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Qqq
 {
@@ -8,10 +9,41 @@ namespace Qqq
         static void Main(string[] args)
         {
             //TestException();
-
-
+            //TestTestTestDispose();
+            TestUsing();
         }
 
+
+        static void TestUsing()
+        {
+            using(FileStream fr = new FileStream("qqq.jpg", FileMode.Open))
+            {
+                try
+                {
+                    byte[] buffer = new byte[fr.Length];
+                    var tmp = fr.Read(buffer, 0, buffer.Length);
+                    Console.WriteLine(tmp);
+                    //foreach (var a in buffer)
+                    //  Console.Write(a + " ");
+                    fr.Dispose();
+                    throw new ObjectDisposedException(nameof(fr));
+                }
+                catch(ObjectDisposedException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                
+            }
+        }
+
+        static void TestTestTestDispose()
+        {
+            DisposeTest dt = new DisposeTest();
+
+            dt.SomeMethod();
+
+            dt.Dispose();
+        }
 
         static void TestException()
         {
