825adb42ab0729ec6303e3f9d134b89ba45c1210
Егор Богданов <egor5803@bk.ru>
2022-03-11 04:34:42 +0300

    new

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace Qqq
 {
@@ -8,10 +10,43 @@ namespace Qqq
        
         static void Main(string[] args)
         {
-
-            ChillIterators.TestIterator();
+            CheckList();
+            //CheckPrint();
+            //ChillIterators.TestIterator();
             //ThrowParamsTest.TestThrowFinal();
         }
+
+
+        static void CheckList()
+        {
+            var list = new List<int>() { 1, 2, 3 };
+
+            foreach (var item in list)
+                list.Add(0);
+            /*
+             var item = list.GetEnumerator();
+             while(item.MoveNext()) {
+             int tmp = item.Current;
+            }
+             */
+
+        }
+        static void CheckPrint()
+        {
+            int i = 0;
+            var data = Enumerable.Range(0, 5).Select(x =>
+            {
+                i++;
+                return x;
+            });
+
+            Console.WriteLine(data.Count());
+            Console.WriteLine(data.Max());
+            Console.WriteLine(data.Min());
+
+            Console.WriteLine(i);
+        }
+
         
     }
 }
